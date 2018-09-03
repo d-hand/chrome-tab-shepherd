6776e8d699ce74eceff6ba7d1ee8a0e839e21aee
d_hand <d_hand@skbkontur.ru>
2018-09-03 21:07:34 +0500

    запилил перехватывающие обработчики и выделил стиля для iframe

--- content.js
+++ content.js
@@ -1,48 +1,51 @@
-class TabShepherdUserMessage {
+class TabShepherdKeyHandler {
     static __init() {
-        TabShepherdUserMessage.onShow = () => {}
-        TabShepherdUserMessage.onHide = () => {}
-        TabShepherdUserMessage.onArrowRight = () => {}
-        TabShepherdUserMessage.onArrowLeft = () => {}
-        TabShepherdUserMessage.onArrowUp = () => {}
-        TabShepherdUserMessage.onArrowDown = () => {}                
+        TabShepherdKeyHandler.onShow = () => {}
+        TabShepherdKeyHandler.onHide = () => {}
+        TabShepherdKeyHandler.onArrowRight = () => {}
+        TabShepherdKeyHandler.onArrowLeft = () => {}
+        TabShepherdKeyHandler.onArrowUp = () => {}
+        TabShepherdKeyHandler.onArrowDown = () => {}                
 
-        TabShepherdUserMessage.__wasShown = false
-        TabShepherdUserMessage.arrowHandlerMap = {
-            ArrowUp: () => TabShepherdUserMessage.onArrowUp(),
-            ArrowDown: () => TabShepherdUserMessage.onArrowDown(),            
-            ArrowLeft: () => TabShepherdUserMessage.onArrowLeft(),
-            ArrowRight: () => TabShepherdUserMessage.onArrowRight()
+        TabShepherdKeyHandler.__wasShown = false
+        TabShepherdKeyHandler.arrowHandlerMap = {
+            ArrowUp: () => TabShepherdKeyHandler.onArrowUp(),
+            ArrowDown: () => TabShepherdKeyHandler.onArrowDown(),            
+            ArrowLeft: () => TabShepherdKeyHandler.onArrowLeft(),
+            ArrowRight: () => TabShepherdKeyHandler.onArrowRight()
         }
 
-        document.body.onkeydown = TabShepherdUserMessage.__documentOnKeyDown
-        document.body.onkeyup = TabShepherdUserMessage.__documentOnKeyUp
+        window.addEventListener('keydown', TabShepherdKeyHandler.__documentOnKeyDown, true)
+        window.addEventListener('keyup', TabShepherdKeyHandler.__documentOnKeyUp, true)
     }
 
     static __documentOnKeyDown(e) {
-        if (e.code === "KeyQ" && e.ctrlKey === true && !TabShepherdUserMessage.__wasShown) {
-            TabShepherdUserMessage.__wasShown = true
-            TabShepherdUserMessage.onShow()
+        if (e.code === "Backquote" && e.ctrlKey === true && !TabShepherdKeyHandler.__wasShown) {
+            TabShepherdKeyHandler.__wasShown = true
+            TabShepherdKeyHandler.onShow()
+            e.stopPropagation()
         } 
 
-        if (TabShepherdUserMessage.__wasShown && TabShepherdUserMessage.arrowHandlerMap[e.code])
-            TabShepherdUserMessage.arrowHandlerMap[e.code]()
+        if (TabShepherdKeyHandler.__wasShown && TabShepherdKeyHandler.arrowHandlerMap[e.code]) {
+            TabShepherdKeyHandler.arrowHandlerMap[e.code]()
+            e.stopPropagation()
+        }
     }
 
     static __documentOnKeyUp(e) {
-        if (TabShepherdUserMessage.__wasShown &&  e.key === "Control") {
-            TabShepherdUserMessage.__wasShown = false
-            TabShepherdUserMessage.onHide()
+        if (TabShepherdKeyHandler.__wasShown &&  e.key === "Control") {
+            TabShepherdKeyHandler.__wasShown = false
+            TabShepherdKeyHandler.onHide()
         }
     }
 }
-TabShepherdUserMessage.__init()
+TabShepherdKeyHandler.__init()
 
 //debugger;
 
 /*
     TODO 
-        0) ширина каждого элемента должна зависить от ширены экрана
+        0) надо зафиксировать ширину вкладки, точно можно будте сказать сколько вкладок влезит на экран 
         1) подобрать огненный шрифт
         2) переписать это говно на риакт (c Webpack-окм кончено)
         3) дефолтный favicon и screenShot
@@ -50,9 +53,10 @@ TabShepherdUserMessage.__init()
         5) выравнить title по середине favicon-а?
         6) сделать background persistent=true, а то кажется из-за этого теряется вся инфа о вкладках
         7) что делать если вкладок слишком много ?)
-        8) строка детализации полный url или title
+        8) строка детализации полный url или title ?)
         9) добваить тень!)
-        10) все переписать %) если вкладок много то подтормаживает getTabs и выпилить лоудер
+        11) обработать клик мыши 
+        12) по нормальному считать  rowOffset
 */
 
 var tabs = undefined
@@ -61,7 +65,6 @@ var selectedIndex = undefined
 
 var iframe = document.createElement('iframe')
 iframe.style.cssText = `
-    display: none;
     position: fixed;
     top: 5%;
     left: 5%;
@@ -75,7 +78,7 @@ iframe.style.cssText = `
 
 var style = document.createElement('style');
 style.type = 'text/css';
-var rules = document.createTextNode(`
+style.appendChild(document.createTextNode(`
     .loader {
         border: 16px solid #f3f3f3;
         border-radius: 50%;
@@ -84,50 +87,70 @@ var rules = document.createTextNode(`
         height: 120px;  
         animation: spin 2s linear infinite;
     }
-    
     @keyframes spin {
         0% { transform: rotate(0deg); }
         100% { transform: rotate(360deg); }
-    }`);    
-style.appendChild(rules);
+    }
+
+    .tabList {
+        width: 100%;
+        height: 100%;
+    
+        display: flex;
+        flex-direction: row;                
+        flex-wrap: wrap;                    /* Переносить элементы wrap или нет nowrap (еще бывыает wrap-reverse) */
+        justify-content: space-around;      /* выравнивает flex-элементы по главной оси текущей строки flex-контейнера */
+        align-items: center;                /* Flex-ЭЛЕМЕНТЫ могут быть выровнены вдоль поперечной оси текущей строки flex-контейнера, подобно justify-content, но в перпендикулярном направлении.*/
+        align-content: space-around;        /* Выравнивает СТРОКИ flex-контейнера во flex-контейнере, когда есть дополнительное пространство по поперечной оси, подобно тому,  как justify-content выравнивает отдельные элементы по главной оси. */   
+    
+        background:  rgba(0, 0, 0, 0.0);    
+    }
+
+    .tabItem {
+        background: rgba(0, 0, 0, 0.0);
+        width: 256px;
+        border-width: 2px;
+        border-style: solid;
+        border-radius: 15px;       
+    }
+
+    .tabItem-selected {
+        border-color: blue;
+    }
+
+    .titleContainer {
+        background: #cecece;
+        border-bottom: 1px solid black;
+        border-radius: 15px 15px 0px 0px;
+        padding: 3%;
+        white-space: nowrap;        /* Запрещаем перенос строк */
+        overflow: hidden;           /* Обрезаем все, что не помещается в область */
+        text-overflow: ellipsis;    /* Добавляем многоточие */
+    }
+
+    .favicon {
+        margin-right: 4px;
+    }
+
+    .screenShot {
+        border-radius: 0px 0px 15px 15px;
+    }
+    
+`));
+
 var loader = document.createElement('div')
 loader.classList.add('loader')
 
 var tabList = document.createElement('div')
-tabList.style.cssText = `    
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    flex-direction: row;                
-    flex-wrap: wrap;                    /* Переносить элементы wrap или нет nowrap (еще бывыает wrap-reverse) */
-    justify-content: space-around;      /* выравнивает flex-элементы по главной оси текущей строки flex-контейнера */
-    align-items: center;                /* Flex-ЭЛЕМЕНТЫ могут быть выровнены вдоль поперечной оси текущей строки flex-контейнера, подобно justify-content, но в перпендикулярном направлении.*/
-    align-content: space-around;        /* Выравнивает СТРОКИ flex-контейнера во flex-контейнере, когда есть дополнительное пространство по поперечной оси, подобно тому,  как justify-content выравнивает отдельные элементы по главной оси. */   
-
-    background:  rgba(0, 0, 0, 0.0);
-`
+tabList.classList.add('tabList')
 
 iframe.onload = function() {
-    iframe.contentDocument.body.appendChild(tabList)
-    iframe.contentDocument.getElementsByTagName("head")[0].appendChild(style);
-    
-    TabShepherdUserMessage.onShow = () => show()
-    TabShepherdUserMessage.onHide  = () => hide()
-    TabShepherdUserMessage.onArrowRight = () => selectNext()
-    TabShepherdUserMessage.onArrowLeft = () => selectPrevious()
-    TabShepherdUserMessage.onArrowUp = () => selectTop()
-    TabShepherdUserMessage.onArrowDown = () => selectBottom()
-    
-};  
-document.body.appendChild(iframe)
-
-
-function show() {
     tabList.innerHTML = ''
-    tabList.appendChild(loader);
-    iframe.style.display = 'block';
+    iframe.contentDocument.getElementsByTagName("head")[0].appendChild(style);    
+    iframe.contentDocument.body.appendChild(tabList)
     
+    tabList.appendChild(loader);
+
     chrome.runtime.sendMessage(chrome.runtime.id, {getTabs: true}, (response) => {
         tabList.removeChild(loader)
         tabs = response        
@@ -136,14 +159,10 @@ function show() {
         calculateRowOffset()        
     });
 
+
     function createTabItem(tab) {
         var tabItem = document.createElement('div')
-        tabItem.style.cssText = `
-            background: rgba(0, 0, 0, 0.0);
-            width: 256px;
-            border: 1px solid black;
-            border-radius: 15px;
-        `
+        tabItem.classList.add('tabItem')
         tabItem.appendChild(createTitleContainer(tab))
         tabItem.appendChild(createScreenShot(tab))            
         return tabItem
@@ -152,19 +171,11 @@ function show() {
 
     function createTitleContainer(tab) {
         var container = document.createElement('div')
-        container.style.cssText = `
-            background: #cecece;
-            border-bottom: 1px solid black;
-            border-radius: 15px 15px 0px 0px;
-            padding: 3%;
-            white-space: nowrap;        /* Запрещаем перенос строк */
-            overflow: hidden;           /* Обрезаем все, что не помещается в область */
-            text-overflow: ellipsis;    /* Добавляем многоточие */
-        `
+        container.classList.add('titleContainer')
+
         var favicon = document.createElement('img')
-        favicon.style.cssText = `
-            margin-right: 4px;
-        `
+        favicon.classList.add('favicon')
+
         favicon.src = tab.favIconUrl || ""
         favicon.width = 16
         favicon.height = 16
@@ -179,37 +190,51 @@ function show() {
 
     function createScreenShot(tab) {
         var screenShot = document.createElement('img')
-        screenShot.style.cssText = `
-        border-radius: 0px 0px 15px 15px;
-        `
+        screenShot.classList.add('screenShot')
         screenShot.src = tab.screenShotDataUrl || ""
-        screenShot.width = 256 //todo надо в % 
-        screenShot.height = 128
+        screenShot.width = 256 
+        screenShot.height = 128 
 
         return screenShot
     }
+
+    function calculateRowOffset() {    
+        let bottom = tabList.children[0].getBoundingClientRect().bottom;
+        for (let i = 0; i < tabList.children.length; ++i) {
+            let top = tabList.children[i].getBoundingClientRect().top
+            if (top > bottom) {
+                rowOffset = i
+                return;            
+            }
+        }    
+    }    
+};
+
+
+TabShepherdKeyHandler.onShow = () =>  {
+    document.body.appendChild(iframe)
 }
 
-function hide() {
-    iframe.style.display = 'none';
+TabShepherdKeyHandler.onHide  = () => {
+    document.body.removeChild(iframe)
     if (tabs && tabs[selectedIndex])
-        chrome.runtime.sendMessage(chrome.runtime.id, { selectedTabId: tabs[selectedIndex].tabId });
+        chrome.runtime.sendMessage(chrome.runtime.id, { selectedTabId: tabs[selectedIndex].tabId });    
 }
 
-function selectNext() {    
+TabShepherdKeyHandler.onArrowRight = () => {
     selectNewTabItem(selectedIndex + 1)
 }
 
-function selectPrevious() {
+TabShepherdKeyHandler.onArrowLeft = () => {
     selectNewTabItem(selectedIndex - 1)
 }
 
-function selectTop() {
+TabShepherdKeyHandler.onArrowUp = () => {
     var newSelectedIndex = selectedIndex - rowOffset
-    selectNewTabItem(newSelectedIndex >= 0 ? newSelectedIndex : selectedIndex)
+    selectNewTabItem(newSelectedIndex >= 0 ? newSelectedIndex : selectedIndex)    
 }
 
-function selectBottom() {
+TabShepherdKeyHandler.onArrowDown = () => {
     var newSelectedIndex = selectedIndex + rowOffset
     selectNewTabItem(newSelectedIndex < tabList.children.length ? newSelectedIndex : tabList.children.length - 1)
 }
@@ -221,19 +246,8 @@ function selectNewTabItem(index) {
 
     var selectedTabItem = tabList.children[selectedIndex]
     if (selectedTabItem) 
-        selectedTabItem.style.border = newSelectedTabItem.style.border
+        selectedTabItem.classList.remove('tabItem-selected')
 
-    newSelectedTabItem.style.border = "2px solid blue"    
+    newSelectedTabItem.classList.add('tabItem-selected')
     selectedIndex = index    
-}
-
-function calculateRowOffset() {    
-    let bottom = tabList.children[0].getBoundingClientRect().bottom;
-    for (let i = 0; i < tabList.children.length; ++i) {
-        let top = tabList.children[i].getBoundingClientRect().top
-        if (top > bottom) {
-            rowOffset = i
-            return;            
-        }
-    }    
 }
